8bbc27bb2df1427b3c2e2a6c0924ea1d94a8953a
aminkhm1997 <aminkhm097@gmail.com>
2024-08-30 18:22:31 +0330

    add app pages

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # local
-    'accounts.apps.AccountsConfig'
+    'accounts.apps.AccountsConfig',
+    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [str(BASE_DIR.joinpath('templates'))],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from pages.views import HomePageView , AboutUsPageView
+
+urlpatterns = [
+    path('' , HomePageView.as_view(), name='home'),
+    path('about-us/' , AboutUsPageView.as_view(), name='about_us'),
+
+  ]
