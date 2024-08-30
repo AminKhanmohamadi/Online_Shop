7656588de6cdf03e09430928f172e608c0961d78
aminkhm1997 <aminkhm097@gmail.com>
2024-08-30 18:41:38 +0330

    add crispy form

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party
+    "crispy_forms",
+    "crispy_bootstrap5",
+
     # local
     'accounts.apps.AccountsConfig',
     'pages.apps.PagesConfig'
@@ -138,3 +142,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+
+
+# Crispy forms settings
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
